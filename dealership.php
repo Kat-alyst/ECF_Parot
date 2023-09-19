31d362767be9a8e7f163e57d51387865f6947ad9
Katalyst <kensadiak@gmail.com>
2023-09-19 11:59:09 +0100

    first commit

---
+++ dealership.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&family=Titillium+Web:ital,wght@0,200;0,300;1,200&display=swap" 
+rel="stylesheet">
+<link rel = "reset stylesheet" href="css/reset.css">
+<link rel = "stylesheet" href= "css/main.css">
+    <title>Dealearship</title>
+</head>
+
+<body>
+    <header class="header-main">
+        <div class="header-main-logo">
+            <img src="img\logo_black.png" alt="garage parot logo">
+            <nav class="header-main-nav">
+                <ul>       
+                <li><a href="index.php">HOME</a></li>
+                <li><a href="services.php">SERVICES</a></li>
+                <li><a href="#">MAINTENANCE</a></li>
+                <li><a href="#">DEALERSHIP</a></li>
+                <li><a href="about.php">ABOUT</a></li>
+                <li><a href="contact.php">CONTACT</a></li>
+                </ul>
+            </nav>
+</div>
+</header>
+<footer></footer>
+</body>
---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&family=Titillium+Web:ital,wght@0,200;0,300;1,200&display=swap" 
+    rel="stylesheet">
+    <link rel = "reset stylesheet" href="css/reset.css">
+    <link rel = "stylesheet" href= "css/main.css">
+    <title>Garage V. Parot</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+
+    <header class="header">
+        <a href="index.php" class="logo">
+        <img src="img\logo_black.png" alt="" style="width: 150px">
+        </a>
+
+        <nav class="navbar">
+            <a href="index.php" style="" class="active">Home</a>
+            <a href="repair.php" style="">Repair</a>
+            <a href="buy.php" style="">Buy</a>
+            <a href="reviews.php" style="">Reviews</a>
+            <a href="contact.php" style="">Contact</a>
+            <a href="about.php" style="">About</a>
+            <a href="signup.php" class="btnSignup">Sign Up</a>
+        </nav>
+
+        <div class="social-media">
+            <a href="#" style="--i:1"><i class='bx bxl-twitter'></i></a>
+            <a href="#" style="--i:2"><i class='bx bxl-facebook'></i></a>
+            <a href="" style="--i:3"><i class='bx bxl-instagram-alt'></i></a>
+            
+        </div>
+    </header>
+
+    <section class="home">
+        <div class="home-content">
+            <h1>WELCOME TO <br>GARAGE VINCENT PAROT.<br></h1>
+            <h3>THE FUTURE OF DRIVING!</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium aliquam neque eaque eum commodi assumenda exercitationem quisquam aperiam facilis labore nemo, repellendus perferendis, autem velit libero rem ullam.</p>
+            <a href="repair.php" class="btnRepair">Repair your Car</a>
+            <a href="buy.php" class="btnBuy">Buy a Car</a>
+            
+        </div>
+
+        <div class="">
+            <div class="">
+                <img src="#" alt="">
+            </div>
+        </div>
+
+        <div class=""></div>
+    </section>
+
+
+</body>
+
+</html>
