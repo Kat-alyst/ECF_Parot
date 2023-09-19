ab7fb9ed01579eea8b255ccc45a9e349e1df0be5
Katalyst <kensadiak@gmail.com>
2023-09-19 11:50:08 +0100

    about.php

---
+++ about.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@300&family=Titillium+Web:ital,wght@0,200;0,300;1,200&display=swap" 
+    rel="stylesheet">
+    <link rel = "reset stylesheet" href="css/reset.css">
+    <link rel = "stylesheet" href= "css/main.css">
+    <link rel = "reset stylesheet" href="css\about.css">
+    <title>Garage V. Parot</title>
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+
+<body>
+
+    <header class="header">
+        <a href="index.php" class="logo">
+        <img src="img\logo_black.png" alt="" style="width: 150px">
+        </a>
+
+        <nav class="navbar">
+            <a href="index.php" style="" class="active">Home</a>
+            <a href="repair.php" style="">Repair</a>
+            <a href="buy.php" style="">Buy</a>
+            <a href="reviews.php" style="">Reviews</a>
+            <a href="about.php" style="">About</a>
+            <a href="contact.php" style="">Contact</a>
+            <a href="signup.php" class="btnSignup">Sign Up</a>
+        </nav>
+
+        <div class="social-media">
+            <a href="#" style="--i:1"><i class='bx bxl-twitter'></i></a>
+            <a href="#" style="--i:2"><i class='bx bxl-facebook'></i></a>
+            <a href="" style="--i:3"><i class='bx bxl-instagram-alt'></i></a>
+            
+        </div>
+    </header>
+
+<section id="about">
+      
+      <div class="aboutParot">
+      
+        <img src="img\vincent_parot.webp">
+      </div>
+      <p>Hello!
+        My name is Vincent Parot.<br><br>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+        Placeat velit expedita, quam eveniet, mollitia dignissimos id ipsam, 
+        ullam maiores nesciunt voluptate sequi reprehenderit debitis 
+        voluptatum ea modi rerum molestias doloremque.
+      </p> 
+
+    </section>
+
+    
